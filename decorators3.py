c6f094800011d69137326e3894c03fdc85fad223
michallibov <92799624+michallibov@users.noreply.github.com>
2022-05-25 19:00:19 +0300

    Update decorators3.py
    I added an option to pass a string to the function.

--- decorators3.py
+++ decorators3.py
@@ -2,24 +2,24 @@ import decorator
 
 
 @decorator.decorator
-def execute_twice_decorator(function):
+def execute_twice_decorator(function: callable, *args):
     """
     This decorator gets a function and executes it twice.
     :param function: the function we want to execute twice.
     :return: the function we passed from the main. In the first time
     it will just call it, but in the second execution it will return it.
     """
-    function()
-    return function()
+    function(*args)
+    return function(*args)
 
 
 @execute_twice_decorator
-def print_hello() -> None:
+def print_hello(name=None) -> None:
     """
     This function prints "Hello!" to the terminal.
     """
-    print("Hello!")
+    print("Hello " + name + "!") if name else print("Hello!")
 
 
 if __name__ == '__main__':
-    print_hello()
+    print_hello("name")
