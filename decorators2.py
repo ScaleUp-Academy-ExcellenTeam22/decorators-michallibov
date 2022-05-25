2bc9469734c11ed4a1cbfac147c373615af40a2d
michallibov <92799624+michallibov@users.noreply.github.com>
2022-05-25 16:32:56 +0300

    Update decorators2.py

--- decorators2.py
+++ decorators2.py
@@ -1,25 +1,27 @@
+from functools import wraps
+
 
 def surprise_decorator(function):
     """
     This is the decorator that calls the wrapper which call the function that prints "Surprise".
     :param function: the function we want to call (in this case- surprise()).
-    :return: the value of the wrapper which is the function that prints "Surprise".
+    :return: the execution of the wrapper. The wrapper prints "Surprise!".
     """
-    def surprise_wrapper():
+    @wraps(function)
+    def surprise_wrapper() -> None:
         """
-        This wrapper calls (when returning) the function we passed which in this case is surprise().
-        :return: the function which in this case is surprise().
+        This wrapper prints "Surprise!"
         """
-        return function()
+        print("Surprise!")
     return surprise_wrapper
 
 
 @surprise_decorator
 def surprise() -> None:
     """
-    This function prints "Surprise" to the terminal.
+    This function is not reachable.
     """
-    print("Surprise!")
+    print("This function is not reachable.")
 
 
 if __name__ == '__main__':
